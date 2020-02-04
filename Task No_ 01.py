6dfe2c130e765422ce0f5c636c113c09656cc99c
Komal8754 <60584853+Komal8754@users.noreply.github.com>
2020-02-04 03:31:34 -0800

    Task No: 01

---
+++ Task No_ 01.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+name= 'Fariha Shah'
+age='22'
+degreename= 'BE Petroleum and Gas'
+university= 'Dawood University of Engineering and Technology'
+classenrolledin= 'AI fro everyone'
+print("name:", name)
+print("age:", age)
+print("degreename:", degreename)
+print("university:", university)
+print("classenrolledin:", classenrolledin)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
